9508ca40f5fb0cb00cb24234c28b851a23d2ba8f
Mike Santoro <mike.santoro42@gmail.com>
2019-07-04 13:43:17 -0400

    users page retrieving from database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
   get 'pages/about'
   get 'default/index'
   get 'default/signup'
+  get 'signup', to: 'users#new'
 
   root 'default#index'
+
+  resources :users
 
 end
