676fdf228614ca70a68110d0d330551986bbc916
Mike Santoro <mike.santoro42@gmail.com>
2019-07-05 13:26:54 -0400

    login working - navbar changing - session delete on browser close

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
-  end 
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 
 end
