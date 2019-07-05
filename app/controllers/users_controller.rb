cae4cbf2393c0e0fdf4c06fd7fb95293bffa6022
Mike Santoro <mike.santoro42@gmail.com>
2019-07-05 10:13:05 -0400

    user creation working - error messages for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def index
-    @user = User.new 
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # view the users profile
+      flash[:success] = "success"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-  get 'pages/about'
-  get 'default/index'
-  get 'default/signup'
-  get 'signup', to: 'users#new'
+  # GET  
+  get '/users/new'
+  get '/pages/about'
+  get '/default/index'
+  get '/default/signup'
+  get '/signup', to: 'users#new'
+
+  #POST
+  post '/signup', to: 'users#create'
 
   root 'default#index'
 
